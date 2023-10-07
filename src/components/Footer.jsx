9da4ac0109a8d1822e7e1149a69aa49c5015c328
HafizhMA <muhammadazhr29@gmail.com>
2023-10-07 18:47:24 +0700

    footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <div className="w-full text-center bg-slate-400 text-white p-4">
+        <p className="text-lg font-bold">Copyright 2023</p>
+      </div>
+    </>
+  );
+};
+
+export default Footer;
