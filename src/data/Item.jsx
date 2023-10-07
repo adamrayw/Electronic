c23a32d8cd32700009b74c34a74b609f0bb68326
HafizhMA <muhammadazhr29@gmail.com>
2023-10-07 18:46:08 +0700

    add item array object

---
+++ src/data/Item.jsx
@@ -0,0 +1,47 @@
+import camera from "../assets/camera.jpg";
+import handphone from "../assets/handphone.jpg";
+import keyboard from "../assets/keyboard.jpg";
+import laptop from "../assets/laptop.jpg";
+import monitor from "../assets/monitor.jpg";
+import mouse from "../assets/mouse.jpg";
+
+const items = [
+  {
+    id: 1,
+    barang: "Camera",
+    img: camera,
+    harga: 300000,
+  },
+  {
+    id: 2,
+    barang: "Handphone",
+    img: handphone,
+    harga: 500000,
+  },
+  {
+    id: 3,
+    barang: "Keyboard",
+    img: keyboard,
+    harga: 100000,
+  },
+  {
+    id: 4,
+    barang: "Laptop",
+    img: laptop,
+    harga: 1500000,
+  },
+  {
+    id: 5,
+    barang: "Monitor",
+    img: monitor,
+    harga: 700000,
+  },
+  {
+    id: 6,
+    barang: "Mouse",
+    img: mouse,
+    harga: 80000,
+  },
+];
+
+export default items;
