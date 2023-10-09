e21dc3b3be237935149f2dd484dfd60c4b4d8690
HafizhMA <muhammadazhr29@gmail.com>
2023-10-09 19:32:46 +0700

    rename variabel, using localstorage

--- src/features/CartSlice.jsx
+++ src/features/CartSlice.jsx
@@ -4,7 +4,7 @@ import items from "../data/Item";
 
 const initialState = {
   products: items,
-  cartItems: [], 
+  cartItems: JSON.parse(localStorage.getItem("cartItems")) || [], 
 };
 
 const cartSlice = createSlice({
@@ -20,6 +20,7 @@ const cartSlice = createSlice({
       } else {
         state.cartItems.push({ ...newItem, quantity: 1 });
       }
+      localStorage.setItem("cartItems", JSON.stringify(state.cartItems));
     },
     addOneQuantity: (state,action) => {
       const newItem = action.payload;
@@ -28,6 +29,7 @@ const cartSlice = createSlice({
       if (existingItem){
         existingItem.quantity +=1;
       }
+      localStorage.setItem("cartItems", JSON.stringify(state.cartItems));
     },
     deleteOneQuantity: (state,action) => {
       const newItem = action.payload;
@@ -36,17 +38,19 @@ const cartSlice = createSlice({
       if (existingItem){
         existingItem.quantity -=1;
         if (existingItem.quantity < 1){
-          state.cartItems = state.cartItems.filter((item) => item.id !== deleteOneSpecificItem.id);
+          state.cartItems = state.cartItems.filter((item) => item.id !== newItem.id);
         }
       }
+      localStorage.setItem("cartItems", JSON.stringify(state.cartItems));
     },
     deleteOneProduct: (state,action) => {
       const newItem = action.payload;
       const existingItem = state.cartItems.find((item) => item.id === newItem.id);
       
       if (existingItem){
-        state.cartItems = state.cartItems.filter((item) => item.id !== deleteOneSpecificProduct.id);
+        state.cartItems = state.cartItems.filter((item) => item.id !== newItem.id);
       }
+      localStorage.setItem("cartItems", JSON.stringify(state.cartItems));
     },
     // dibawah code yang lebih efektif 
     // updateCartItem: (state, action) => {
