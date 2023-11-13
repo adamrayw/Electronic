acd677191580301f22cf4ca9cc3415660d0c0fa3
HafizhMA <muhammadazhr29@gmail.com>
2023-11-13 19:06:38 +0700

    refactor

--- src/components/ProductCheckout.jsx
+++ src/components/ProductCheckout.jsx
@@ -37,96 +37,59 @@ const ProductCheckout = ({ item, handleAddOneQuantity, handleDeleteOneQuantity, 
         onChange={handleCheckboxChange}
       />
       {/* Render the quantity and price based on the checked property */}
-      {item.checked ? (
-        <>
-          <img
-            className="object-cover w-[30%] h-32"
-            src={item.img}
-            alt={item.namaBarang}
-          />
-          <div className="bodycard ms-3">
-            <h3 className="font-bold">{item.namaBarang}</h3>
-            <p>Harga: Rp {item.hargaBarang.toLocaleString()}</p>
-          </div>
-          <div className="totalbarang ms-auto">
+      <img
+        className="object-cover w-[30%] h-32"
+        src={item.img}
+        alt={item.namaBarang}
+      />
+      <div className="bodycard ms-3">
+        <h3 className="font-bold">{item.namaBarang}</h3>
+        <p>Harga: Rp {item.hargaBarang.toLocaleString()}</p>
+      </div>
+      <div className="totalbarang ms-auto">
+        {item.checked ? (
+          <>
             <p>Total Barang: {item.quantity}</p>
             <p>
               Total Harga: Rp{" "}
               {(item.hargaBarang * item.quantity).toLocaleString()}
             </p>
-            <div className="flex">
-              <button
-                className="bg-blue-300 me-2 p-1 rounded"
-                onClick={handleAddOneQuantity}
-              >
-                + 1
-              </button>
-              <input
-                type="number"
-                className="w-10 me-2"
-                value={quantityInput}
-                onChange={handleQuantityInputChange}
-              />
-              <button
-                className="bg-blue-300 p-1 me-2 rounded"
-                onClick={handleDeleteOneQuantity}
-              >
-                - 1
-              </button>
-              <button
-                className="bg-blue-300 p-1 rounded"
-                onClick={handleDeleteOneItem}
-              >
-                hapus
-              </button>
-            </div>
-          </div>
-        </>
-      ) : (
-        <>
-          <img
-            className="object-cover w-[30%] h-32"
-            src={item.img}
-            alt={item.namaBarang}
-          />
-          <div className="bodycard ms-3">
-            <h3 className="font-bold">{item.namaBarang}</h3>
-            <p>Harga: Rp {item.hargaBarang.toLocaleString()}</p>
-          </div>
-          <div className="totalbarang ms-auto">
+          </>
+        ) : (
+          <>
             <p>Total Barang: 0</p>
             <p>
               Total Harga: Rp 0
             </p>
-            <div className="flex">
-              <button
-                className="bg-blue-300 me-2 p-1 rounded"
-                onClick={handleAddOneQuantity}
-              >
-                + 1
-              </button>
-              <input
-                type="number"
-                className="w-10 me-2"
-                value={quantityInput}
-                onChange={handleQuantityInputChange}
-              />
-              <button
-                className="bg-blue-300 p-1 me-2 rounded"
-                onClick={handleDeleteOneQuantity}
-              >
-                - 1
-              </button>
-              <button
-                className="bg-blue-300 p-1 rounded"
-                onClick={handleDeleteOneItem}
-              >
-                hapus
-              </button>
-            </div>
-          </div>
-        </>
-      )}
+          </>
+        )}
+        <div className="flex">
+          <button
+            className="bg-blue-300 me-2 p-1 rounded"
+            onClick={handleAddOneQuantity}
+          >
+            + 1
+          </button>
+          <input
+            type="number"
+            className="w-10 me-2"
+            value={quantityInput}
+            onChange={handleQuantityInputChange}
+          />
+          <button
+            className="bg-blue-300 p-1 me-2 rounded"
+            onClick={handleDeleteOneQuantity}
+          >
+            - 1
+          </button>
+          <button
+            className="bg-blue-300 p-1 rounded"
+            onClick={handleDeleteOneItem}
+          >
+            hapus
+          </button>
+        </div>
+      </div>
     </div>
   )
 }
