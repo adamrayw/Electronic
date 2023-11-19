8008baeb011779c46276dfdcfa9b062e9bc20803
HafizhMA <muhammadazhr29@gmail.com>
2023-11-19 16:54:48 +0700

    tidy up the UI appearance

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -35,11 +35,17 @@ const Card = () => {
 
   return (
     <div className="container mx-auto py-[100px]">
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-5">
-        {filteredProducts.map((item) => (
-          <ProductCard key={item.id} item={item} onAddToCart={() => handleAddToCart(item)} />
-        ))}
-      </div>
+      {filteredProducts.length > 0 ? (
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-5">
+          {filteredProducts.map((item) => (
+            <ProductCard key={item.id} item={item} onAddToCart={() => handleAddToCart(item)} />
+          ))}
+        </div>
+      ) : (
+        <div className="flex mt-52 justify-center h-screen w-full">
+          <p className="font-bold text-xl">Barang tidak ada</p>
+        </div>
+      )}
     </div>
   );
 };
