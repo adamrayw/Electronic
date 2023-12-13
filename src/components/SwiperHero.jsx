1e697296d0856f4e6806b32f35afe2fc2702d02c
HafizhMA <muhammadazhr29@gmail.com>
2023-12-13 16:31:19 +0700

    init swiper and make carousel using swiper

---
+++ src/components/SwiperHero.jsx
@@ -0,0 +1,41 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { useState, useEffect } from 'react';
+import { allProducts } from '../data/Item';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+
+// import required modules
+import { Navigation } from 'swiper/modules';
+
+const SwiperHero = () => {
+    const [products, setProducts] = useState([]);
+
+    const fetchData = async () => {
+        try {
+            const allProductsData = await allProducts();
+            setProducts(allProductsData);
+        } catch (error) {
+            console.error("Error fetching data:", error);
+        }
+    };
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    return (
+        <>
+            <Swiper navigation={true} modules={[Navigation]} >
+                {products.map(product => (
+                    <SwiperSlide key={product.id}>
+                        <img className='w-full h-full object-cover' src={product.img} alt={`Slide`} />
+                    </SwiperSlide>
+                ))}
+            </Swiper>
+        </>
+    )
+}
+
+export default SwiperHero;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,12 +3,13 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 import Card from "../components/Card";
 import Hero from "../components/Hero";
+import SwiperHero from "../components/SwiperHero";
 
 const Home = () => {
   return (
     <>
       <Header />
-      <Hero />
+      <SwiperHero />
       <Card />
       <Footer />
     </>
