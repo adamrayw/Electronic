96b8b4a15def06d7b445b8f4947651ad44959634
HafizhMA <muhammadazhr29@gmail.com>
2023-12-18 18:56:11 +0700

    wishlist and share

--- src/components/detail-product/DetailProduct.jsx
+++ src/components/detail-product/DetailProduct.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { BsCart4 } from "react-icons/bs";
+import { MdFavoriteBorder } from "react-icons/md";
+import { CiShare2 } from "react-icons/ci";
 
 const DetailProduct = () => {
     return (
         <>
-            <div className="container lg:mx-10 max-w-[73rem]  grid lg:grid-cols-3 md:grid-cols-1 gap-4 mt-[5rem] ">
+            <div className="container lg:mx-10 max-w-[73rem]  grid lg:grid-cols-3 md:grid-cols-1 gap-4 mt-[5rem] pb-20">
                 <div className="img-detail">
                     <img src="https://images.unsplash.com/photo-1527864550417-7fd91fc51a46?auto=format&fit=crop&q=80&w=1767&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
                 </div>
@@ -34,7 +36,11 @@ const DetailProduct = () => {
                             <BsCart4 className='me-2' />
                             Keranjang
                         </button>
-                        <button className='bg-sky-700 text-white w-full p-1 rounded'>Beli</button>
+                        <button className='bg-sky-700 text-white w-full p-1 rounded mb-2'>Beli</button>
+                        <div className='flex justify-around p-1'>
+                            <button className='flex items-center'><MdFavoriteBorder className='me-2' size={20} /> Wishlist</button>
+                            <button className='flex items-center'><CiShare2 className='me-2' size={20} /> Share</button>
+                        </div>
                     </div>
                 </div>
             </div>
