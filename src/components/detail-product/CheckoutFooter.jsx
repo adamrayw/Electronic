f57635729bc09c80fb201c313113276f70742627
HafizhMA <muhammadazhr29@gmail.com>
2024-01-02 16:10:32 +0700

    make checkout footer and refactor some code

---
+++ src/components/detail-product/CheckoutFooter.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { BsCart4 } from "react-icons/bs";
+import { MdFavoriteBorder } from "react-icons/md";
+import { CiShare2 } from "react-icons/ci";
+
+const CheckoutFooter = () => {
+    return (
+        <>
+            <div className='flex gap-4 justify-center'>
+                <CiShare2 size={40} />
+                <button className='text-slate-700 flex justify-center items-center mb-2 w-full border-solid border-2 border-slate-600 p-1 rounded'>
+                    <BsCart4 className='me-2' />
+                    Keranjang
+                </button>
+                <button className='bg-slate-600 justify-center items-center text-white w-full p-1 rounded mb-2'>Beli</button>
+                <MdFavoriteBorder size={40} />
+            </div>
+        </>
+    )
+}
+
+export default CheckoutFooter
