d68d282aeb2e6b8661c6836e4fc00228d28cec60
HafizhMA <muhammadazhr29@gmail.com>
2024-01-09 15:05:45 +0700

    remove use navigate

--- src/components/register-login/Register.jsx
+++ src/components/register-login/Register.jsx
@@ -1,11 +1,10 @@
 import { useState } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import { register } from '../../services/apiServices';
 
 const Register = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const navigate = useNavigate();
 
     const handleResetInput = () => {
         setEmail('')
