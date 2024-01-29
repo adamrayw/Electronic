d2744d35da89ecb8af9d65739f4435b58d857193
HafizhMA <muhammadazhr29@gmail.com>
2024-01-29 20:10:33 +0700

    forget passaword form

--- src/components/register-login/ResetPassword.jsx
+++ src/components/register-login/ResetPassword.jsx
@@ -3,16 +3,18 @@ import React from 'react'
 const ResetPassword = () => {
     return (
         <div className='h-screen flex justify-center items-center'>
-            <form action="submit">
-                <div>
-                    <p>lupa password</p>
+            <form action="submit" className='border-2 border-slate-600 p-4 rounded shadow-2xl max-w-md'>
+                <div className='text-center mb-4'>
+                    <p className='font-bold'>Atur ulang kata sandi</p>
+                    <p>Masukkan e-mail yang terdaftar. Kami akan mengirimkan kode verifikasi untuk atur ulang kata sandi.
+                    </p>
                 </div>
                 <div>
-                    <label htmlFor="email">Email</label>
-                    <input type="email" />
+                    <label htmlFor="email" className='block font-semibold'>Email</label>
+                    <input type="email" className='w-full py-1 border-2 border-slate-300' />
                 </div>
-                <div>
-                    <button>submit</button>
+                <div className='text-center mt-4 mb-2'>
+                    <button className='bg-slate-600 rounded p-1 text-white font-semibold w-full'>Submit</button>
                 </div>
             </form>
         </div>
